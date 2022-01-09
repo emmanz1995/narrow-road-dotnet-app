26f73b1e3025d6b6323a3e97a7601a932f9ba6f0
emmanz95 <emmanza2@gmail.com>
2022-01-09 21:21:21 +0000

    new changes

--- server/src/controller/testimony.controller.js
+++ server/src/controller/testimony.controller.js
@@ -1,7 +1,12 @@
 const Testimony = require('../model/testimony.model');
+const Prayer = require('../model/prayer.model');
 const expressAsyncHandler = require('express-async-handler');
 
-const createTestimony = expressAsyncHandler(async (req, res) => {})
+const createTestimony = expressAsyncHandler(async (req, res) => {
+    const { title, description } = req?.body;
+    try {
+    } catch(error) {}
+})
 
 module.exports = {
     createTestimony
